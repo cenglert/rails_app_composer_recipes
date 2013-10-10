835d5d4c9a7857edd9167ff2d5db75ff4ce619d2
Chris Englert <chris@blastingpath.com>
2013-10-10 14:12:13 -0400

    no comment

--- recipes/general.rb
+++ recipes/general.rb
@@ -1,17 +1,17 @@
 ## Ruby on Rails
+after_bundler do
+  HOST_OS = RbConfig::CONFIG['host_os']
+  say_wizard "Running the general"
+  say_wizard "Your operating system is #{HOST_OS}."
+  say_wizard "You are using Ruby version #{RUBY_VERSION}."
+  say_wizard "You are using Rails version #{Rails::VERSION::STRING}."
+  
+  repo = 'https://raw.github.com/cenglert/application_builds/master/'
+  
+  insert_into_file 'config/environments/development.rb', "config.action_mailer.default_url_options = { :host => 'localhost:3000' }", :before => "\nend"
 
-HOST_OS = RbConfig::CONFIG['host_os']
-say_wizard "Running the general"
-say_wizard "Your operating system is #{HOST_OS}."
-say_wizard "You are using Ruby version #{RUBY_VERSION}."
-say_wizard "You are using Rails version #{Rails::VERSION::STRING}."
 
-repo = 'https://raw.github.com/cenglert/application_builds/master/'
-
-insert_into_file 'config/environments/development.rb', "config.action_mailer.default_url_options = { :host => 'localhost:3000' }", :before => "\nend"
-
-
-
+end
 __END__
 
 name: general
