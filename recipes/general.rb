4e448673af6e0507fb2909a1e793b18f4cbb4c04
Chris Englert <chris@blastingpath.com>
2013-10-10 14:09:08 -0400

    added in a recipe for general application changes

---
+++ recipes/general.rb
@@ -0,0 +1,22 @@
+## Ruby on Rails
+
+HOST_OS = RbConfig::CONFIG['host_os']
+say_wizard "Running the general"
+say_wizard "Your operating system is #{HOST_OS}."
+say_wizard "You are using Ruby version #{RUBY_VERSION}."
+say_wizard "You are using Rails version #{Rails::VERSION::STRING}."
+
+repo = 'https://raw.github.com/cenglert/application_builds/master/'
+
+insert_into_file 'config/environments/development.rb', "config.action_mailer.default_url_options = { :host => 'localhost:3000' }", :before => "\nend"
+
+
+
+__END__
+
+name: general
+description: "General application changes"
+author: Chris Englert
+
+run_after: []
+category: configuration
