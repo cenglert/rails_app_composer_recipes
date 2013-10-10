e5ab6deb46463f26a16fb3648d41cdabd926935d
Chris Englert <chris@blastingpath.com>
2013-10-10 11:19:50 -0400

    static

--- recipes/static.rb
+++ recipes/static.rb
@@ -7,7 +7,14 @@ say_wizard "You are using Rails version #{Rails::VERSION::STRING}."
 
 
 repo = 'https://raw.github.com/cenglert/application_builds/master/'
-
-insert_into_file 'config/routes.rb', "\n  match ':action' => 'static#:action'", :before => "\nend"
+route_text = '\n match "action" => "static#action"\n'
+insert_into_file 'config/routes.rb', route_text, :before => "\nend"
 
 __END__
+
+name: static
+description: "Add static page functionality to your applicaiton"
+author: Chris Englert
+
+run_after: []
+category: configuration
